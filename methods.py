3e49c1111a62e3bb5870cda03a4456e2981e5adc
Deepprakash <deepprakashp354@gmail.com>
2015-06-10 15:19:26 +0530

    testing github

---
+++ methods.py
@@ -0,0 +1,10 @@
+def move(time,speed):
+	print("i am moving on" + str(speed) + " kmph at " + str(time))
+	distance=speed*time;
+	print("distance = " + str(distance))
+
+move (10,5)
+print("______________________")
+move(20,10)
+print("______________________")
+move(5,10)
